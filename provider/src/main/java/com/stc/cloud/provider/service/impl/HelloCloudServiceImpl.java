f9e5e7367f214ac1106213dcc7eb9b18402f7cfc
suntianchi <824284773@qq.com>
2020-10-22 15:53:29 +0800

    增加nacos配置中心

--- provider/src/main/java/com/stc/cloud/provider/service/impl/HelloCloudServiceImpl.java
+++ provider/src/main/java/com/stc/cloud/provider/service/impl/HelloCloudServiceImpl.java
@@ -1,6 +1,8 @@
 package com.stc.cloud.provider.service.impl;
 
 import com.stc.cloud.provider.service.HelloCloudService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,9 +14,14 @@ import org.springframework.stereotype.Service;
  * @since 2020/10/20
  */
 @Service(value = "HelloCloudServiceImpl")
+@RefreshScope
 public class HelloCloudServiceImpl implements HelloCloudService {
+
+    @Value("${test}")
+    private String userName;
+
     @Override
     public void helloCloud(String str) {
-        System.out.println("hello: " + str);
+        System.out.println("hello: " + str + userName);
     }
 }
