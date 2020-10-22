1defe4078e53de59fca6cd2a5e31b32f1326c033
suntianchi <824284773@qq.com>
2020-10-22 11:57:02 +0800

    增加eureka集群配置

--- consumer/src/main/java/com/stc/cloud/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/stc/cloud/consumer/ConsumerApplication.java
@@ -3,10 +3,12 @@ package com.stc.cloud.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaClient
 @EnableFeignClients
 public class ConsumerApplication {
--- provider/src/main/java/com/stc/cloud/provider/ProviderApplication.java
+++ provider/src/main/java/com/stc/cloud/provider/ProviderApplication.java
@@ -3,11 +3,13 @@ package com.stc.cloud.provider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("com.stc.cloud.provider.mapper")
+@EnableConfigServer
 public class ProviderApplication {
 
 	public static void main(String[] args) {
